a9620b876377720a1d899c3e8a42e310762bc60b
Evgeny Ignatenko <ier2000@yandex.ru>
2020-09-03 19:34:53 +0700

    Create list navigation

--- lab1/os_lab1.cpp
+++ lab1/os_lab1.cpp
@@ -1,105 +1,175 @@
 #include <iostream>
 #include <fstream>
+#include <cassert>
 #include <string>
 
 typedef unsigned int uint;
 
-std::string filename("data.dat");
+static constexpr char* filename((char*)"data.dat");
 
-// class Node 
-// {
-// public:
-//     Node(){};
-//     Node(std::string name, uint mark) : _next(NULL)   
-//     {
-//         this->_name = name;
-//         this->_mark = mark;    
-//     }
-   
-//     Node* next() const { return _next; }
-//     std::string name() const { return _name; }
-//     uint mark() const { return _mark; }
-//     void info() const { std:: cout << "\n Name: " << _name << "\n Mark: " << _mark; }
-
-//     void set_next(Node* node) { _next = node; }
-
-// private:
-//     Node* _next; 
-//     std::string _name;
-//     uint _mark;
-// };
-
-struct Node 
+class StudentMarks
 {
-    Node() : next(NULL) {};
-    Node(std::string name, uint mark) : next(NULL)   
+private:
+    struct Node
     {
-        this->name = name;
-        this->mark = mark;    
+        Node() : next(NULL) {};
+        Node(char* name, uint mark) : next(NULL)
+        {
+            this->name = name;
+            this->mark = mark;
+        }
+
+        void info() const { std:: cout << "\n " << name << ": " << mark; }
+
+        Node* next;
+        char* name;
+        uint mark;
+    };
+
+    Node* head;
+    Node* tail;
+
+public:
+    StudentMarks() : head(NULL), tail(NULL) {}
+    void print_list() const { for (Node* n = head; n != NULL; n = n->next) n->info(); }
+
+    void append(char* name, uint mark) {
+        if (head == NULL) { head = new Node(name, mark); tail = head; }
+        else { tail->next = new Node(name, mark); tail = tail->next; }
+    }
+    void append(Node* n) {
+        if (head == NULL) { head = new Node(*n); tail = head; }
+        else { tail->next = new Node(*n); tail = tail->next; }
     }
 
-    void info() const { std:: cout << "\n Name: " << name << "\n Mark: " << mark; }
+    void insert(char* name, uint mark, uint pos) {
+        if (pos == 0) {
+            Node* new_node = new Node(name, mark);
+            new_node->next = head;
+            head = new_node;
+        } else {
+            Node* n = head;
+            uint count = 0;
+            
+            while (count < pos - 1)
+            {
+                assert(n->next != NULL);
+                n = n->next;
+                count++;
+            }
+            
+            Node* new_node = new Node(name, mark);
+            new_node->next = n->next;
+            n->next = new_node;
+        }    
+    }
 
-    Node* next; 
-    std::string name;
-    uint mark;
+    void remove(uint pos) {
+        if (pos == 0) {
+            Node* t = head;
+            head = head->next;
+            delete t;
+        } else {
+            Node* n = head;
+            uint count = 0;
+            
+            while (count < pos - 1)
+            {
+                assert(n->next != NULL);
+                n = n->next;
+                count++;
+            }
+            
+            Node* t = n->next;
+            n->next = t->next;
+            delete t;
+        }    
+    }
+
+    void operator[](uint pos) const
+    {
+        Node* n = head;
+        uint count = 0;
+
+        while (count != pos)
+        {
+            assert(n->next != NULL);
+            n = n->next;
+            count++;
+        }
+        n->info();
+    }
+
+    void free_list() { 
+        Node* t;
+        Node* n = head;
+        while(n != NULL) 
+        {
+            t = n->next;
+            delete n;
+            n = t; 
+        }
+        head = NULL; 
+        tail = NULL;
+    }
+
+    void serialize() const
+    {
+        std::ofstream ofs(filename);
+        
+        Node* n = head;
+        while (n != NULL)
+        {
+            ofs.write(reinterpret_cast<char*>(n), sizeof(*n));
+            n = n->next;
+        }
+
+        ofs.close();
+    }   
+
+    void deserialize()
+    {
+        free_list();
+        std::ifstream ifs(filename);
+
+        while(!ifs.eof()) {
+            Node* n = new Node;
+            if(ifs.read(reinterpret_cast<char*>(n), sizeof(*n)).gcount() < sizeof(*n)) { delete n; break; }   
+            append(n);
+            delete n;
+        }
+
+        ifs.close();
+    }
 };
 
-void serialize(Node* n)
-{
-    std::ofstream ofs(filename);
-    while (n != NULL)
-    {
-        ofs.write(reinterpret_cast<char*>(n), sizeof(*n));
-        n = n->next;
-    }
-
-    ofs.close();
-}   
-
-Node* deserialize()
-{
-    Node* head = NULL;
-    Node* n;
-    Node* m;
-    
-    std::ifstream ifs(filename);
-
-    while(1)
-    {
-        n = new Node;
-        ifs.read(reinterpret_cast<char*>(n), sizeof(*n));      
-        if (ifs.gcount() < sizeof(Node)) break;
-        
-        if (head == NULL) 
-            head = n;
-        else 
-            m->next = n;
-    
-        m = n;
-    }
-
-    ifs.close();
-    return head;
-}   
 
 int main()
 {
-    Node* n1 = new Node("Mark", 4);
-    Node* n2 = new Node("Stas", 5);
-    Node* n3 = new Node("Vika", 3);
-    n1->next = n2 ; n2->next = n3;
+    StudentMarks sm;
+    sm.append((char*)"Max", 4);
+    sm.append((char*)"Julia", 5);
+    sm.append((char*)"Ugin", 3);
+
+    sm.insert((char*)"Pavel", 5, 0);
+    sm.insert((char*)"Stepan", 2, 2);
     std::cout << "\n Before serialization: ";
-    n1->info();
-    n2->info();
-    n3->info();
+    sm.print_list();
 
-    serialize(n1);
+    sm.serialize();
+    sm.deserialize();
 
-    std::cout << "\n After serialization: ";
-    Node* head = deserialize();
-    for(Node* n = head; n != NULL; n = n->next)
-        n->info();
+    std::cout << "\n\n After serialization: ";
+    sm.print_list();
 
+    std::cout << "\n\n Second student: ";
+    sm[2];
+
+    sm.remove(2);
+
+    std::cout << "\n\n Result: ";
+    sm.print_list();
+
+    sm.free_list();
     return 0;
 }
